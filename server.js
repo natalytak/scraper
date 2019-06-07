161ee3fb7ad0f689fb084b539eb3595e0537616a
Tasha <natalytak@yahoo.com>
2019-06-06 21:03:48 -0400

    fixed port

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+const port = process.env.PORT || 3000;
+
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/hackernews";
 mongoose.connect(MONGODB_URI);
 
@@ -29,6 +31,6 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
+app.listen(port, function() {
+  console.log(`App running on port ${port}!`);
 });
